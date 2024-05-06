3079f5b1378cb0136547354e70a4310d41c41125
Florian "sp1rit"​ <sp1ritCS@protonmail.com>
2024-05-06 16:29:17 +0200

    tests: fixed unit tests after
    f0adc0d changed how Rectangles are defined and thus the test wasn't
    working anymore

--- src/tests.rs
+++ src/tests.rs
@@ -6,7 +6,7 @@ use std::io::Read;
 fn parse_document() {
 	let vm = TabulaVM::new("../tabula-java/target/tabula-1.0.6-SNAPSHOT-jar-with-dependencies.jar", true).unwrap();
 	let env = vm.attach().unwrap();
-	let areas: Vec<(i32, Rectangle)> = vec![(ABSOLUTE_AREA_CALCULATION_MODE, Rectangle::from_coords(150.56,58.9,654.7,536.12))];
+	let areas: Vec<(i32, Rectangle)> = vec![(ABSOLUTE_AREA_CALCULATION_MODE, Rectangle::from_coords(58.9, 150.56, 654.7, 596.12))];
 	let tabula = env.configure_tabula(Some(&areas), Some(&vec![1]), OutputFormat::Csv, false, ExtractionMethod::Decide, false, None).unwrap();
 	let mut file = tabula.parse_document(&std::path::Path::new("./test_data/spanning_cells.pdf"), "test_spanning_cells").unwrap();
 	let mut fin = String::new();
