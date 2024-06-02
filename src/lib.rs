475684a4bb2144cd518efe6e44d53497b0395f31
John Beisley <johnbeisleyuk@gmail.com>
2024-06-02 12:50:28 +0100

    tests: added tests for the new tempfile implementation
    On Linux, this tests both temporary file implemenations.

--- src/lib.rs
+++ src/lib.rs
@@ -61,7 +61,8 @@ pub use objects::{RELATIVE_AREA_CALCULATION_MODE, ABSOLUTE_AREA_CALCULATION_MODE
 
 use anyhow::Result;
 use jni::{AttachGuard, InitArgsBuilder, JNIEnv, JNIVersion, JavaVM, objects::{JObject, JValue}, errors::Error as JError};
-pub use jni; // reexport
+pub use jni;
+use tmp_file::TempFile; // reexport
 
 use std::result::Result as StdResult;
 use std::ops::Deref;
@@ -209,13 +210,13 @@ impl Tabula<'_> {
 	/// `descriptor_name` refers to the filename passed to [memfd_create()](https://git.kernel.org/pub/scm/docs/man-pages/man-pages.git/tree/man2/memfd_create.2)
 	///
 	pub fn parse_document(&self, path: &Path, descriptor_name: &str) -> Result<std::fs::File> {
-		let output = tmp_file::TmpFile::new(descriptor_name)?;
+		let output = tmp_file::new(descriptor_name)?;
 
 		let output_path = output.get_path();
 
 		self.parse_document_into(path, &output_path)?;
 		
-		let file = output.get_file();
+		let file = output.into_file();
 		Ok(file)
 	}
 
--- src/tmp_file.rs
+++ src/tmp_file.rs
@@ -3,12 +3,30 @@ use std::fs::File;
 
 use anyhow::Result;
 
-#[cfg(target_os = "linux")]
-pub struct TmpFile(i32);
+pub trait TempFile where Self: Sized {
+	fn new<T: AsRef<str>>(name: T) -> Result<Self>;
+
+	fn get_path(&self) -> PathBuf;
+
+	fn into_file(self) -> File;
+}
 
 #[cfg(target_os = "linux")]
-impl TmpFile {
-	pub fn new<T: AsRef<str>>(name: T) -> Result<Self> {
+pub fn new<T: AsRef<str>>(name: T) -> Result<impl TempFile> {
+	LinuxMemFd::new(name)
+}
+
+#[cfg(not(target_os = "linux"))]
+pub fn new<T: AsRef<str>>(name: T) -> Result<impl TempFile> {
+	GenericTempFile::new(name)
+}
+
+#[cfg(target_os = "linux")]
+pub struct LinuxMemFd(i32);
+
+#[cfg(target_os = "linux")]
+impl TempFile for LinuxMemFd {
+	fn new<T: AsRef<str>>(name: T) -> Result<Self> {
 		let name = std::ffi::CString::new(name.as_ref())?;
 		let fd = unsafe {
 			libc::syscall(libc::SYS_memfd_create, name.as_ptr(), 0) as i32
@@ -17,7 +35,7 @@ impl TmpFile {
 		Ok(Self(fd))
 	}
 
-	pub fn get_path(&self) -> PathBuf {
+	fn get_path(&self) -> PathBuf {
 		let mut path = PathBuf::new();
 		path.push("/proc");
 		path.push(std::process::id().to_string());
@@ -26,28 +44,68 @@ impl TmpFile {
 		path
 	}
 
-	pub fn get_file(self) -> File {
+	fn into_file(self) -> File {
 		unsafe { <File as std::os::fd::FromRawFd>::from_raw_fd(self.0) }
 	}
 }
 
-#[cfg(not(target_os = "linux"))]
-pub struct TmpFile(tempfile::NamedTempFile, File);
+pub struct GenericTempFile(tempfile::NamedTempFile, File);
 
-#[cfg(not(target_os = "linux"))]
-impl TmpFile {
-	pub fn new<T: AsRef<str>>(name: T) -> Result<Self> {
+impl TempFile for GenericTempFile {
+	fn new<T: AsRef<str>>(name: T) -> Result<Self> {
 		let prefix = <std::ffi::OsString as std::str::FromStr>::from_str(name.as_ref())?;
 		let tf = tempfile::NamedTempFile::with_prefix(&prefix)?;
 		let f = tf.reopen()?;
 		Ok(Self(tf, f))
 	}
 
-	pub fn get_path(&self) -> PathBuf {
+	fn get_path(&self) -> PathBuf {
 		self.0.path().to_owned()
 	}
 
-	pub fn get_file(self) -> File {
+	fn into_file(self) -> File {
 		self.1
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use std::{fs::File, io::{Read, Write}};
+
+	use super::{TempFile, GenericTempFile};
+
+	#[cfg(target_os = "linux")]
+	#[test]
+	fn test_linux_mem_fd() -> Result<(), anyhow::Error> {
+	use super::LinuxMemFd;
+		common_test::<LinuxMemFd>()
+	}
+
+	#[test]
+	fn generic_temp_file() -> Result<(), anyhow::Error> {
+		common_test::<GenericTempFile>()
+	}
+
+	fn common_test<T: TempFile>() -> Result<(), anyhow::Error> {
+		const TEST_DATA: &[u8] = b"test data";
+
+		let tf = GenericTempFile::new("name")?;
+
+		let path = tf.get_path();
+		let mut write_file = File::create(&path)?;
+		write_file.write_all(TEST_DATA)?;
+		drop(write_file);
+
+		let mut read_data = Vec::new();
+		let mut read_file = tf.into_file();
+		read_file.read_to_end(&mut read_data)?;
+
+		assert_eq!(TEST_DATA, &read_data, "file did not contain the written data");
+
+		drop(read_file);
+
+		assert!(!path.exists(), "temporary file not removed after all usage completed");
+
+		Ok(())
 	}
 }
